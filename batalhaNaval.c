f51c0ab5b3cdae80668e2ff930f76d832fb5b734
felipeprogramadorjppb <frnlima@icloud.com>
2025-09-13 02:03:38 +0000

    dois navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,9 +6,72 @@
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
+
+    #define TAM_TABULEIRO 10   // Tamanho fixo do tabuleiro 10x10
+    #define TAM_NAVIO 3        // Tamanho fixo dos navios
+    #define AGUA 0             // Representa água
+    #define NAVIO 3            // Representa parte do navio
+
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+    
+    // Matriz 10x10 que representa o tabuleiro
+    int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO];
+
+    // Inicializar o tabuleiro com água (0)
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro[i][j] = AGUA;
+        }
+    }
+    // Definir as coordenadas iniciais dos navios
+    // (linha, coluna) - você pode alterar para testar
+    int linha_horizontal = 2, coluna_horizontal = 4; // Navio na horizontal
+    int linha_vertical = 5, coluna_vertical = 7;     // Navio na vertical
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+
+    if (coluna_horizontal + TAM_NAVIO <= TAM_TABULEIRO) {
+        int sobreposicao = 0;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            if (tabuleiro[linha_horizontal][coluna_horizontal + i] == NAVIO) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+        if (!sobreposicao) {
+            for (int i = 0; i < TAM_NAVIO; i++) {
+                tabuleiro[linha_horizontal][coluna_horizontal + i] = NAVIO;
+            }
+        }
+    }
+
+
+    if (linha_vertical + TAM_NAVIO <= TAM_TABULEIRO) {
+        int sobreposicao = 0;
+        for (int i = 0; i < TAM_NAVIO; i++) {
+            if (tabuleiro[linha_vertical + i][coluna_vertical] == NAVIO) {
+                sobreposicao = 1;
+                break;
+            }
+        }
+        if (!sobreposicao) {
+            for (int i = 0; i < TAM_NAVIO; i++) {
+                tabuleiro[linha_vertical + i][coluna_vertical] = NAVIO;
+            }
+        }
+    }
+
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    // Exibir o tabuleiro
+    printf("===== TABULEIRO BATALHA NAVAL =====\n\n");
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
